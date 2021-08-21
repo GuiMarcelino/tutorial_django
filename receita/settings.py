60b2c736b05deecece7b0d66577e3dd9bd573814
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2021-08-21 09:55:44 -0300

    add db mysql

--- receita/settings.py
+++ receita/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'receita.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'receitas',
+        'USER': 'root',
+        'PASSWORD': 'Gui281209',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
