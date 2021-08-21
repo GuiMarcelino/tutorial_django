9f4a2ae714da707ca53c7b3a1c8fb89aa368ddf2
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2021-08-21 10:55:09 -0300

    create model receira

--- site_receitas/models.py
+++ site_receitas/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Receita(models.Model):
+    nome_receita = models.CharField(max_length=255)
+    tempo_preparo = models.TextField()
+    rendimento = models.CharField(max_length=50)
+    categoria = models.CharField(max_length=50)
+    modo_preparo = models.TextField()
+    ingredientes = models.TextField()
+    data_receita = models.DateTimeField(auto_now_add=True)
+
